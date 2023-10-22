8fc78a3b2836ff34d837c6ceb68fbca4ae21573a
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-22 18:51:56 +0600

    Add the 3d desktop

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion' 
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas/'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+        <div className={`${styles.paddingX} absolute insert-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+          <div className='flex flex-col justify-center items-center mt-5'>
+            
+            <div className='w-5 h-5 rounded-full bg-[#915eff]'>
+            </div>
+            <div className='w-1 sm:h-80 h-40 violet-gradient'></div>
+          
+          </div>
+          
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className='text-[#915eff]'> Ibrahim</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I create 3D front-end Web Page <br className='sm:block hidden' />
+              and Back-end Features
+            </p>
+          </div>
+        </div>
+
+        <ComputersCanvas />
+        
+    </section>
   )
 }
 
