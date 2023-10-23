9787b379aa488175ba4365b33f39e3669f68f96f
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-23 18:29:52 +0600

    Correct light effect

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,15 +8,15 @@ const Computers = ({ isMobile }) => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
+      <ambientLight intensity={.65} />
       <spotLight
-        position={[-30, 50, 10]}
+        position={[0, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? .1 : .17}
