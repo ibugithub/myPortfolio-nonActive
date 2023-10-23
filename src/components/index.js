a9c02052929209c1bdfaded79370338a8dffddfd
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-23 16:53:00 +0600

    Remove unnecessary component and comment

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,7 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
-import Experience from "./Experience";
 import Works from "./Works";
-import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
 
@@ -15,9 +13,7 @@ export {
   Navbar,
   About,
   Tech,
-  Experience,
   Works,
-  Feedbacks,
   Contact,
   CanvasLoader,
   EarthCanvas, 
